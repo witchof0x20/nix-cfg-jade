b8b5a578d143de4630beeaff0169b913093ddf25
Jade Harley <jade@witchof.space>
2023-09-07 22:30:52 -0400

    More modules

---
+++ home/easyeffects.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, options, ... }:
+with lib;
+let
+  cfg = config.jade.home.easyeffects;
+in
+{
+  imports = [ ];
+  options = {
+    jade.home.easyeffects = {
+      enable = mkEnableOption "EasyEffects";
+    };
+  };
+  config = mkIf cfg.enable {
+    # Enable the service
+    services.easyeffects = {
+      enable = true;
+      preset = "flat";
+    };
+    # Create an immutable preset
+    xdg.configFile."easyeffects/output/flat.json" = {
+      enable = true;
+      text = toJson {
+        output = {
+          blocklist = [ ];
+          plugin_order = [ ];
+        };
+      };
+    };
+  };
+}
--- home/module.nix
+++ home/module.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, options, ... }:
+{ config, lib, options, osConfig, pkgs, ... }:
 with lib;
 let
   cfg = config.jade.home;
@@ -7,6 +7,8 @@ in
   imports = [
     # .XResources
     ./xresources.nix
+    # EasyEffects
+    ./easyeffects.nix
   ];
   options = {
     jade.home = {
@@ -14,5 +16,28 @@ in
       enable = mkEnableOption "a default set of configurations used for Jade's home-manager setup";
     };
   };
-  config = mkIf cfg.enable { };
+  config = mkIf cfg.enable {
+    # Shell config
+    programs.bash = {
+      enable = true;
+      enableVteIntegration = true;
+      sessionVariables = {
+        # TODO: refer to the specific version of vim created by the vim config
+        EDITOR = "vim";
+      };
+      shellAliases = {
+        # Shortcut for xdg-open
+        # TODO: move this together with xdg
+        open = "${pkgs.xdg-utils}/bin/xdg-open";
+      };
+      # TODO: move this maybe?
+      initExtra = ''
+        PS1="\[$(tput bold)\]\[\033[38;5;0m\]\[\033[48;5;10m\]\u@\h:\w$ \[$(tput sgr0)\] \[$(tput sgr0)\]"
+      '';
+    };
+  };
 }
+
+
+
+
