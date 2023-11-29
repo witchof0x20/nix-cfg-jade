afac89492c4d85d95711e8242c6ce92dee6c5bc8
Jade Harley <jade@witchof.space>
2023-11-29 00:06:47 -0500

    Hash check

--- packages/autoeq/default.nix
+++ packages/autoeq/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
     owner = "jaakkopasanen";
     repo = "AutoEq";
     rev = "0fad4b2ce4b3ceb97f0086c3a0940b4c2473c1f3";
-    sha256 = "1mb3gfg01mj7ajjl1ylw24mnwamcnnibqyqw6rq";
+    sha256 = "";
   };
 
   buildPhase = "";
