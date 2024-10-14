6e4981b4b1503629732614db952e800581352be9
Jade Harley <jade@witchof.space>
2024-10-14 12:36:49 -0400

    Added firefox stuff

--- home/desktop/firefox.nix
+++ home/desktop/firefox.nix
@@ -15,6 +15,46 @@ in
     };
   };
   config = mkIf cfg.enable {
-    home.packages = [ pkgs.firefox-beta ];
+    programs.firefox = {
+      enable = true;
+      package = pkgs.firefox-beta;
+      profiles = {
+        "default" = {
+          isDefault = true;
+          settings = {
+            # Google Safe Browsing
+            # Leaks the browsing history to Google. Note that disabling Safe Browsing
+            # exposes you to a risk of not being stopped from visiting malicious or phishing sites.
+            browser.safebrowsing.enabled = false;
+            browser.safebrowsing.downloads.enabled = false;
+            browser.safebrowsing.malware.enabled = false;
+            # Firefox stats collecting
+            # Stability and performance reports.
+            datareporting.healthreport.service.enabled = false;
+            datareporting.healthreport.uploadEnabled = false;
+            # Usage statistics.
+            toolkit.telemetry.unified = false;
+            toolkit.telemetry.enabled = false;
+            # Encrypted Media Extensions (DRM)
+            # A binary plugin (closed-source) is shipped with Firefox since v38. It enables playback of encrypted media and lets you use e.g. Netflix without Microsoft Silverlight. To completely remove the plugin you would have to install an EME-free build of Firefox.
+            media.eme.enabled = false;
+            media.gmp-eme-adobe.enabled = false;
+            # Firefox Hello
+            # Firefox connects to third-party (Telefonica) servers without asking for permission.
+            loop.enabled = false;
+            # Pocket integration
+            # A third-party service for managing a reading list of articles.
+            browser.pocket.enabled = false;
+            # Search suggestions
+            # Everything you type in the search box is sent to the search engine. Suggestions based on local history will still work.
+            browser.search.suggest.enabled = false;
+            # Geolocation
+            geo.enabled = false;
+            # Adobe Flash
+            plugin.state.flash = 0;
+          };
+        };
+      };
+    };
   };
 }
