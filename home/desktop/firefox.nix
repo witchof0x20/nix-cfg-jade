9475a5bf28518931e66b689a3094c34759768374
Jade Harley <jade@witchof.space>
2025-03-01 22:09:08 -0500

    Browser changes

--- home/desktop/firefox.nix
+++ home/desktop/firefox.nix
@@ -28,13 +28,6 @@ in
             "browser.safebrowsing.enabled" = false;
             "browser.safebrowsing.downloads.enabled" = false;
             "browser.safebrowsing.malware.enabled" = false;
-            # Firefox stats collecting
-            # Stability and performance reports.
-            "datareporting.healthreport.service.enabled" = false;
-            "datareporting.healthreport.uploadEnabled" = false;
-            # Usage statistics.
-            "toolkit.telemetry.unified" = false;
-            "toolkit.telemetry.enabled" = false;
             # Encrypted Media Extensions (DRM)
             # A binary plugin (closed-source) is shipped with Firefox since v38. It enables playback of encrypted media and lets you use e.g. Netflix without Microsoft Silverlight. To completely remove the plugin you would have to install an EME-free build of Firefox.
             "media.eme.enabled" = false;
@@ -58,6 +51,29 @@ in
             "browser.sessionstore.max_resumed_crashes" = -1;
             # Enable extensions enabled in nix
             "extensions.autoDisableScopes" = 0;
+            # Settings from https://github.com/K3V1991/Disable-Firefox-Telemetry-and-Data-Collection
+            "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+            "browser.newtabpage.activity-stream.telemetry" = false;
+            "browser.ping-centre.telemetry" = false;
+            "datareporting.healthreport.service.enabled" = false;
+            "datareporting.healthreport.uploadEnabled" = false;
+            "datareporting.policy.dataSubmissionEnabled" = false;
+            "datareporting.sessions.current.clean" = true;
+            "devtools.onboarding.telemetry.logged" = false;
+            "toolkit.telemetry.archive.enabled" = false;
+            "toolkit.telemetry.bhrPing.enabled" = false;
+            "toolkit.telemetry.enabled" = false;
+            "toolkit.telemetry.firstShutdownPing.enabled" = false;
+            "toolkit.telemetry.hybridContent.enabled" = false;
+            "toolkit.telemetry.newProfilePing.enabled" = false;
+            "toolkit.telemetry.prompted" = 2;
+            "toolkit.telemetry.rejected" = true;
+            "toolkit.telemetry.reportingpolicy.firstRun" = false;
+            "toolkit.telemetry.server" = "";
+            "toolkit.telemetry.shutdownPingSender.enabled" = false;
+            "toolkit.telemetry.unified" = false;
+            "toolkit.telemetry.unifiedIsOptIn" = false;
+            "toolkit.telemetry.updatePing.enabled" = false;
           };
           # Required extensions
           extensions = with pkgs.nur.repos.rycee.firefox-addons; [
