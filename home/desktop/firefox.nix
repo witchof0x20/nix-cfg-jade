05fb6ee31ca4d45fd071e49acd75c3c2e483c2d2
Jade Harley <jade@witchof.space>
2025-03-07 22:40:00 -0500

    Prevent rebuilding firefox

--- home/desktop/firefox.nix
+++ home/desktop/firefox.nix
@@ -17,7 +17,7 @@ in
   config = mkIf cfg.enable {
     programs.firefox = {
       enable = true;
-      package = pkgs.firefox-beta;
+      package = pkgs.firefox-beta-bin;
       profiles = {
         "default" = {
           isDefault = true;
