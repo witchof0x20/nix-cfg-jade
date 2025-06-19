7fe593ddb2002f76b01ec82e17c5a8da884b7a63
Jade Harley <jade@witchof.space>
2025-06-19 12:04:58 -0400

    oops, tls

--- system/services/dns.nix
+++ system/services/dns.nix
@@ -64,29 +64,6 @@ let
           [ ];
     in
     builtins.concatLists (map processResolver names);
-  toBase64URL = text:
-    let
-      inherit (lib) sublist mod stringToCharacters concatMapStrings;
-      inherit (lib.strings) charToInt;
-      inherit (builtins) substring foldl' genList elemAt length concatStringsSep stringLength;
-      lookup = stringToCharacters "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-      sliceN = size: list: n: sublist (n * size) size list;
-      pows = [ (64 * 64 * 64) (64 * 64) 64 1 ];
-      intSextets = i: map (j: mod (i / j) 64) pows;
-      compose = f: g: x: f (g x);
-      intToChar = elemAt lookup;
-      convertTripletInt = sliceInt: concatMapStrings intToChar (intSextets sliceInt);
-      sliceToInt = foldl' (acc: val: acc * 256 + val) 0;
-      convertTriplet = compose convertTripletInt sliceToInt;
-      join = concatStringsSep "";
-      len = stringLength text;
-      nFullSlices = len / 3;
-      bytes = map charToInt (stringToCharacters text);
-      tripletAt = sliceN 3 bytes;
-      head = genList (compose convertTriplet tripletAt) nFullSlices;
-      tail = tripletAt nFullSlices;
-    in
-    join (head ++ [ tail ]);
 in
 {
   options = {
@@ -128,6 +105,7 @@ in
           {
             name = ".";
             # Quad9 (no blocking)  
+            tls-upstream = true;
             forward-addr = (generateResolverList { names = [ "quad9_no_blocking" ]; });
           }
         ];
