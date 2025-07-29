02bed98529b55fca6194e04fa6338e9e7915c769
Jade Harley <jade@witchof.space>
2025-07-28 21:17:45 -0400

    Update theming

--- home/desktop/themes.nix
+++ home/desktop/themes.nix
@@ -25,14 +25,51 @@ in
     ];
     gtk = {
       enable = true;
-      #cursorTheme = {
-      #  package = pkgs.openzone-cursors;
-      #  name = "Openzone Black";
-      #};
-      #theme = {
-      #  package = pkgs.sweet;
-      #  name = "Sweet-Dark";
-      #};
+      # TODO: check if individual gtk versions need cursors
+      cursorTheme = {
+        package = pkgs.openzone-cursors;
+        name = "Openzone Black";
+        size = 16;
+      };
+      # TODO: check if individual versions need theme
+      theme = {
+        package = pkgs.orchis-theme;
+        name = "Orchis-Dark";
+      };
+      # TODO: Font is already set elsewhere, should be preserved but check anyway
+      gtk2.enable = true;
+      gtk3 = {
+        enable = true;
+        extraConfig = {
+          gtk-application-prefer-dark-theme = true;
+        };
+      };
+      gtk4 = {
+        enable = true;
+        extraConfig = {
+          gtk-application-prefer-dark-theme = true;
+        };
+      };
     };
+    qt = {
+      enable = true;
+      platformTheme.name = "gtk3";
+    };
+    home.pointerCursor = {
+      gtk.enable = true;
+      x11.enable = true;
+      package = pkgs.catppuccin-cursors.mochaPeach;
+      name = "Catppuccin-Mocha-Peach-Cursors";
+      size = 20;
+    };
+    # Prefer dark mode
+    dconf =
+      {
+        settings = {
+          "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+          };
+        };
+      };
   };
 }
